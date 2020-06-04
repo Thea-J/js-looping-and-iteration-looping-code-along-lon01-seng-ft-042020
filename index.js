6a54b4193bbf8ef1c4e9674e09e30eacb27c7af6
github username <github email address>
2020-06-04 03:17:51 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+// Returns an array of messages for each name provided to the function
+function writeCards (name, celebration) {
+    let array = [] ;
+for (let i=0; i<name.length; i+=1) {
+    array.push(`Thank you, ${name[i]}, for the wonderful ${celebration} gift!`)
+} return array
+}
+
+// Logs a countdown from the number provided to zero
+function countDown(n) {
+    while (n+1 != 0) {
+    console.log(n);
+    n-=1;
+}
+}
